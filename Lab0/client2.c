a6062381e8754fa805f0476099d89ef2689292ff
TonyPepeBear <41405045+TonyPepeBear@users.noreply.github.com>
2021-09-16 09:45:48 +0000

    Lab0-2

---
+++ Lab0/client2.c
@@ -0,0 +1,26 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main() {
+    struct sockaddr_in server;
+    int sock;
+    char buf[256];
+    bzero(&server, sizeof(server));
+    server.sin_family = PF_INET;
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_port = htons(5678);
+    sock = socket(PF_INET, SOCK_DGRAM, 0);
+
+    while (scanf(" %s", buf) != EOF) {
+        sendto(sock, buf, sizeof(buf), 0, (struct sockaddr *)&server,
+               sizeof(server));
+    }
+
+    close(sock);
+
+    return 0;
+}
